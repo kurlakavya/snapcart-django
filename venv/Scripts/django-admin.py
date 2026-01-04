ca44fe2e0704aa9c07f133d5d41859a648388cee
Kavya <kavyakurla96@gmail.com>
2026-01-04 13:46:14 +0530

    Add requirements.txt for Render deployment

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\kavya\Downloads\ecommerce.github.io-main\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
